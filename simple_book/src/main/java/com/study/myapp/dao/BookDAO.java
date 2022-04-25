5b476051b2fb48db7450df21746702580e95d748
hayeong <hayeong0624@gmail.com>
2022-04-25 22:28:17 +0900

    Spring Framework

---
+++ simple_book/src/main/java/com/study/myapp/dao/BookDAO.java
@@ -0,0 +1,74 @@
+package com.study.myapp.dao;
+
+import static com.study.myapp.dao.JdbcUtil.*;
+import java.sql.*;
+import java.util.*;
+
+import org.springframework.stereotype.Repository;
+
+import com.study.myapp.dto.BookDTO;
+
+@Repository // 객체를 한 번만 생성해서 필요한 곳에 넘겨줌
+public class BookDAO {
+	private Connection con;
+	
+	public List<BookDTO>  select() {
+		List<BookDTO> list = new ArrayList<BookDTO>();
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String SQL = "SELECT * FROM booktbl";
+		
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				BookDTO dto = new BookDTO();
+				dto.setCode(rs.getInt("code"));
+				dto.setTitle(rs.getString("title"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setPrice(rs.getInt("price"));
+				
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstmt);
+			close(con);
+		}
+		
+		return list;
+	}
+	
+	public int insert(BookDTO insertDTO) {
+		int result = 0;
+		
+		PreparedStatement pstmt = null;
+		
+		String SQL = "INSERT INTO booktbl VALUES(?, ?, ?, ?)";
+		
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(SQL);
+			pstmt.setInt(1, insertDTO.getCode());
+			pstmt.setString(2, insertDTO.getTitle());
+			pstmt.setString(3, insertDTO.getWriter());
+			pstmt.setInt(4, insertDTO.getPrice());
+			
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+			close(con);
+		}
+		
+		return result;
+	}
+		
+}
